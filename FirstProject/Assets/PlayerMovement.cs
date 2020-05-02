498be26fbf28a00cc8d913503ea148a2051f2d2a
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2020-05-02 18:55:47 -0400

    Add materials + player movement

---
+++ FirstProject/Assets/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    private bool moveLeft = false;
+    private bool moveRight = false;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            moveRight = true;
+        }
+        if (Input.GetKey("a"))
+        {
+            moveLeft = true;
+        }
+    }
+
+    // Physics update
+    void FixedUpdate ()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (moveRight)
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+            moveRight = false;
+        }
+        if (moveLeft)
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+            moveLeft = false;
+        }
+    }
+}
