2ec6ef598636d65de795c62bca68c7afabc1bb8f
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2020-05-03 14:21:07 -0400

    Add score text

--- FirstProject/Assets/Scripts/PlayerCollision.cs
+++ FirstProject/Assets/Scripts/PlayerCollision.cs
@@ -3,12 +3,14 @@
 public class PlayerCollision : MonoBehaviour {
 
     public PlayerMovement movement;
+    public Score score;
 
     void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Obstacle")
         {
             movement.enabled = false;
+            score.enabled = false;
         }
     }
 }
