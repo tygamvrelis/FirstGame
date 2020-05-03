17159f85b3f2b9af75741eaf72127e737dd9b927
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2020-05-03 18:55:48 -0400

    Add restart upon failure, score UI, and level complete UI

---
+++ FirstProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public PlayerMovement movement;
+    public Score score;
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel ()
+    {
+        StopGame();
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame ()
+    {
+        if (!gameHasEnded)
+        {
+            gameHasEnded = true;
+            StopGame();
+            Debug.Log("Game over");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void StopGame()
+    {
+        movement.enabled = false;
+        score.enabled = false;
+    }
+
+    void Restart ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
--- FirstProject/Assets/Scripts/PlayerCollision.cs
+++ FirstProject/Assets/Scripts/PlayerCollision.cs
@@ -2,15 +2,11 @@
 
 public class PlayerCollision : MonoBehaviour {
 
-    public PlayerMovement movement;
-    public Score score;
-
     void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Obstacle")
         {
-            movement.enabled = false;
-            score.enabled = false;
+            FindObjectOfType<GameManager>().EndGame();
         }
     }
 }
